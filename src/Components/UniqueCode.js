7f39524b0a2b24f525c7b16f850c46dc850438b6
chris-hughes <hughes.c.i@gmail.com>
2020-12-07 12:21:50 +0000

    form logic added

--- src/Components/UniqueCode.js
+++ src/Components/UniqueCode.js
@@ -4,22 +4,29 @@ class UniqueCode extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
     this.props.onCodeInputChange(e.target.value);
   }
 
+  handleSubmit(e){
+    this.props.onCodeSubmit(e)
+    
+  }
+
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <label> ID (4 characters):
           <input type="text" id="name" name="presentationID" required
             minLength="4" maxLength="4" size="10"
             onChange={this.handleChange}
           />
         </label>
-      </div>
+          <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
